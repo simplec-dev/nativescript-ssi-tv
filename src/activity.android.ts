1ab7cc53c1334de8131e1d40976ef6ec0dce56e7
dvnelson72 <dvnelson72@gmail.com>
2023-08-07 23:47:48 -0400

    1.1..x

--- src/activity.android.ts
+++ src/activity.android.ts
@@ -1,4 +1,6 @@
-﻿import {setActivityCallbacks, AndroidActivityCallbacks, ViewBase, Application as app } from "@nativescript/core";
+﻿﻿import {setActivityCallbacks, AndroidActivityCallbacks} from "tns-core-modules/ui/frame";
+import { ViewBase } from "tns-core-modules/ui/core/view-base";
+import * as app from "tns-core-modules/application";
 import { KeyCodes } from "./keycodes";
 
 // The class below is not currently used (also commented in AndroidManifest.xml)
